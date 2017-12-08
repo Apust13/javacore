0b116589dbaab5065ec652e1fddc0290e5985021
Alex Nikitin <alexandr.nikitin@achieve3000.com>
2017-12-08 10:37:08 -0500

    Reader is working like Java6

--- work/src/main/java/com/apust/javacore/work/IO/readerTxt_v6.java
+++ work/src/main/java/com/apust/javacore/work/IO/readerTxt_v6.java
@@ -1,32 +1,27 @@
 package com.apust.javacore.work.IO;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ReadTxtFile {
+public class readerTxt_v6 {
 
     public static void main(String[] args) {
-
-        final String fileName = "/main/resources/testTxt.txt";
+        final String fileName = System.getProperty(("user.dir")) + "/work/src/main/resources/testText.txt";
 
         List<String> strings = readFile(fileName);
 
         for(String s : strings){
             System.out.println(s);
         }
-
-
     }
 
 
     public static List<String> readFile(String fileName) {
         ArrayList<String> result = new ArrayList<>();
         try {
-
             BufferedReader bR = new BufferedReader(new FileReader(fileName));
             String line;
             try {
