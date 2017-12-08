9ddd5c03f88f80e4655c4111668175db2ea7e307
Alex Nikitin <alexandr.nikitin@achieve3000.com>
2017-12-08 10:12:20 -0500

    Reader implements

--- work/src/main/java/com/apust/javacore/work/IO/ReadTxtFile.java
+++ work/src/main/java/com/apust/javacore/work/IO/ReadTxtFile.java
@@ -1,8 +1,45 @@
 package com.apust.javacore.work.IO;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadTxtFile {
 
     public static void main(String[] args) {
 
+        final String fileName = "/main/resources/testTxt.txt";
+
+        List<String> strings = readFile(fileName);
+
+        for(String s : strings){
+            System.out.println(s);
+        }
+
+
+    }
+
+
+    public static List<String> readFile(String fileName) {
+        ArrayList<String> result = new ArrayList<>();
+        try {
+
+            BufferedReader bR = new BufferedReader(new FileReader(fileName));
+            String line;
+            try {
+                while ((line = bR.readLine()) != null) {
+                    result.add(line);
+                }
+            } finally {
+                bR.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
