afb273a8b335c4b47f8bf09f562e44acf7bae5ce
Apust13 <apust@yandex.ru>
2017-12-20 09:45:08 +0300

    Add Selen Python

---
+++ J2SE/src/main/java/com/apust/goJavaOnline_1/generic/App00.java
@@ -0,0 +1,18 @@
+package com.apust.goJavaOnline_1.generic;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by APUST on 12/13/2017.
+ */
+public class App00 {
+
+    @Test
+    public void testGenericType() {
+
+
+
+    }
+
+
+}
---
+++ PythonExamples/pythoncore/App00.py
@@ -0,0 +1,13 @@
+# a=1.1
+# b=2.2
+#
+# print(a+b)
+
+from decimal import *
+a = Decimal('1.1')
+b = Decimal('2.2')
+print(a+b)
+
+c = Decimal(1.1)
+d = Decimal(2.2)
+print(c+d)
